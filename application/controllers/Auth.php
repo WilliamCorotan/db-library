718572bd4cf764b57244be1ff4c54007b871507f
WilliamCorotan <williamcorotan@gmail.com>
2023-05-30 15:33:09 +0800

    added meaningful comments

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -22,36 +22,38 @@ class Auth extends CI_Controller
      */
     public function login()
     {
-        $this->form_validation->set_rules(
-            'email',
-            'Email',
-            'required|valid_email',
-            array(
-                'valid_email' => 'Please provide a valid %s.'
-            )
-        );
+        // Form validation rules
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email', array('valid_email' => 'Please provide a valid %s.'));
+
+        // Checks if validation rules is not met
         if ($this->form_validation->run() === FALSE) {
             $json_response['form_errors'] = $this->form_validation->error_array();
             exit(json_encode($json_response));
         } else {
+
+            // Data from the post stream
             $form_data = array(
                 'email' => $this->input->post('email', TRUE),
                 'password' => $this->input->post('password', TRUE)
             );
+
+            // Verify and stores user credentials
             $authenticated_user = $this->user_model->verify_credentials($form_data['email'], $form_data['password']);
 
+            // Check if the a record is returned from the database
             if (!empty($authenticated_user)) {
-                $json_response['authenticated_user'] = $authenticated_user;
-                $json_response['message'] = 'Logged in successfully!';
-
+                // Stores user data to session
                 $this->session->set_userdata($authenticated_user);
                 $this->session->set_userdata('is_logged_in', TRUE);
 
-
-                $json_response['session'] = $this->session->userdata();
+                //Stores useful information to the Response Body
+                $json_response['authenticated_user'] = $authenticated_user;
+                $json_response['message'] = 'Logged in successfully!';
+                $json_response['location'] = '/';
                 exit(json_encode($json_response));
             }
 
+            // Returns Response Body when Credentials not match
             $json_response['error_message'] = 'Invalid credentials, please try again!';
             exit(json_encode($json_response));
         }
@@ -73,39 +75,33 @@ class Auth extends CI_Controller
      */
     public function register()
     {
-        $this->form_validation->set_rules(
-            'email',
-            'Email',
-            'required|valid_email|is_unique[user.email]',
-            array(
-                'valid_email' => 'Please provide a valid %s.',
-                'is_unique' => 'This %s is already taken.'
-            )
-        );
+        // Form validation rules
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[user.email]', array('valid_email' => 'Please provide a valid %s.', 'is_unique' => 'This %s is already taken.'));
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|max_length[255]');
-        $this->form_validation->set_rules(
-            'confirm_password',
-            'Confirm Password',
-            'required|matches[password]',
-            array(
-                'matches' => 'The input provided didn\'t match the password.'
-            )
-        );
+        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]', array('matches' => 'The input provided didn\'t match the password.'));
 
+        // Checks if validation rules is not met
         if ($this->form_validation->run() === FALSE) {
             $json_response['form_errors'] = $this->form_validation->error_array();
             exit(json_encode($json_response));
         } else {
+            // Data from the post stream
             $form_data = array(
                 'email' => $this->input->post('email', TRUE),
                 'password' => password_hash($this->input->post('password', TRUE), PASSWORD_BCRYPT)
             );
+
+            // Tries to insert the data to the database
             try {
                 $this->user_model->insert($form_data);
+
+                // Returns the Response Body on insert success
                 $json_response['message'] = 'Successfully registered user';
                 $json_response['location'] = 'login';
                 exit(json_encode($json_response));
-            } catch (\Exception $e) {
+            }
+            // Returns the Response Body when an error 
+            catch (\Exception $e) {
                 $json_response['error_message'] = $e;
                 exit(json_encode($json_response));
             }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -5,12 +5,19 @@ class User_model extends CI_Model
 
     /**
      * Creates new record in the database 
+     * @param string $data
      */
     public function insert($data)
     {
         return $this->db->insert('user', $data);
     }
 
+    /**
+     * Verify the user credentials in the database
+     * @param string $email
+     * @param string $password
+     * @return array user data | empty array
+     */
     public function verify_credentials($email, $password)
     {
         $query = $this->db->select('*')
