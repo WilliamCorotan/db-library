fd6647441034307b468df5a5c070c868ac05a593
WilliamCorotan <williamcorotan@gmail.com>
2023-06-05 15:29:50 +0800

    separated javascript for user profile

--- application/views/pages/profile/index.php
+++ application/views/pages/profile/index.php
@@ -121,51 +121,6 @@
     </article>
 </section>
 
-<script>
-    $(document).ready(function() {
-        // Personal Information Form submit event
-        $('#personal-information-form').on('submit', function(event) {
-            event.preventDefault();
-            console.log($(this).serialize())
-            $.ajax({
-                type: "post",
-                url: "profile/update/user",
-                data: $(this).serialize(),
-                dataType: "json",
-                success: function(response) {
-                    console.log(response);
-                }
-            });
-        });
+<script src="assets/js/profile/user.js">
 
-        // Address Information Form submit event
-        $('#address-information-form').on('submit', function(event) {
-            event.preventDefault();
-            $.ajax({
-                type: "post",
-                url: "profile/update/address",
-                data: $(this).serialize(),
-                dataType: "json",
-                success: function(response) {
-                    console.log(response);
-
-                }
-            });
-        });
-
-        //Security Information Form submit event
-        $('#security-information-form').on('submit', function(event) {
-            event.preventDefault();
-            $.ajax({
-                type: "post",
-                url: "profile/update/security",
-                data: $(this).serialize(),
-                dataType: "json",
-                success: function(response) {
-                    console.log(response);
-
-                }
-            });
-        });
-    });
 </script>
