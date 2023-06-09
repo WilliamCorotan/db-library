34acee243095cfb5311aba75189a03dd0989e509
WilliamCorotan <williamcorotan@gmail.com>
2023-06-09 10:20:58 +0800

    minor hotfix

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -29,45 +29,54 @@ class Book extends CI_Controller
         $this->load->library('upload', $config);
         $this->upload->initialize($config);
 
+        $form_data = array(
+            'title' => $this->input->post('title'),
+            'description' => $this->input->post('description'),
+            'subject_id' => $this->input->post('subject'),
+            'call_number' => $this->input->post('call_number'),
+            'publish_date' => $this->input->post('publish_date'),
+            'publisher_id' => $this->input->post('publisher'),
+            'borrow_status_id' => 1
+        );
+
         if ($this->upload->do_upload('cover_image')) {
-            $form_data = array(
-                'title' => $this->input->post('title'),
-                'description' => $this->input->post('description'),
-                'cover_image' =>  $this->upload->data('file_name'),
-                'subject_id' => $this->input->post('subject'),
-                'call_number' => $this->input->post('call_number'),
-                'publish_date' => $this->input->post('publish_date'),
-                'publisher_id' => $this->input->post('publisher')
-            );
 
-            if (!empty($this->author_model->get($this->input->post('author')))) {
-                $form_data['author_id'] = $this->input->post('author');
-            } else {
-                $author =  $this->author_model->insert($this->input->post('author'));
-                $form_data['author_id'] = $author;
-            }
-
-
-            if (!empty($this->subject_model->get($this->input->post('subject')))) {
-                $form_data['subject_id'] = $this->input->post('subject');
-            } else {
-                $subject = $this->subject_model->insert($this->input->post('subject'));
-                $form_data['subject_id'] = $subject;
-            }
-
-
-            if (!empty($this->publisher_model->get($this->input->post('publisher')))) {
-                $form_data['publisher_id'] = $this->input->post('publisher');
-            } else {
-                $publisher = $this->publisher_model->insert($this->input->post('publisher'));
-                $form_data['publisher_id'] = $publisher;
-            }
-            $json_response['location'] = base_url('admin/books');
-            exit(json_encode($json_response));
+            $form_data['cover_image'] = $this->upload->data('file_name');
         } else {
-            $errors = array('error' => $this->upload->display_errors());
-            $json_response['message'] = $errors;
-            exit(json_encode($json_response));
+            $form_data['cover_image'] = '01-no-cover.jpg';
         }
+
+        if (!empty($this->author_model->get($this->input->post('author')))) {
+            $form_data['author_id'] = $this->input->post('author');
+        } else {
+            $author =  $this->author_model->insert($this->input->post('author'));
+            $form_data['author_id'] = $author;
+        }
+
+
+        if (!empty($this->subject_model->get($this->input->post('subject')))) {
+            $form_data['subject_id'] = $this->input->post('subject');
+        } else {
+            $subject = $this->subject_model->insert($this->input->post('subject'));
+            $form_data['subject_id'] = $subject;
+        }
+
+
+        if (!empty($this->publisher_model->get($this->input->post('publisher')))) {
+            $form_data['publisher_id'] = $this->input->post('publisher');
+        } else {
+            $publisher = $this->publisher_model->insert($this->input->post('publisher'));
+            $form_data['publisher_id'] = $publisher;
+        }
+
+        $this->book_model->insert($form_data);
+        $json_response['location'] = base_url('admin/books');
+        exit(json_encode($json_response));
+    }
+
+    public function fetch()
+    {
+        $response  = $this->load->view('components/books/book_table');
+        return $response;
     }
 }
