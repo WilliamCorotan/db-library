dab8fd88e0d8de611d767915cc090c67b46e0bec
WilliamCorotan <williamcorotan@gmail.com>
2023-06-13 16:57:36 +0800

    removed datatables cdn

--- application/views/partials/admin_header.php
+++ application/views/partials/admin_header.php
@@ -18,10 +18,6 @@
      <!-- FontAwesome CDN -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-     <!-- DataTables CDN -->
-     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
-
-     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 
      <!-- Custom JavaScript -->
      <script src="<?= base_url('assets/js/index.js') ?>"></script>
