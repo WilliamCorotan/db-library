dae5ec72ee6958c8cb887be73d26a7267ca30938
WilliamCorotan <williamcorotan@gmail.com>
2023-06-13 17:34:33 +0800

    changed chartjs to highcharts

--- application/views/pages/admin/index.php
+++ application/views/pages/admin/index.php
@@ -4,37 +4,18 @@
         <span>Here is what's happening with your projects today:</span>
     </div>
 
-    <div class="grid grid-cols-3 gap-3 mb-5">
+    <div class="grid grid-cols-2 gap-3 mb-5">
 
         <div class=" bg-white p-0">
-            <div>
-                <h4 class="fw-semibold text-center text-md-start">User Status</h4>
-            </div>
-            <div id="users-card">
-                <canvas id="users-chart"></canvas>
+            <div id="users-card" class="w-100">
             </div>
         </div>
 
         <div class=" bg-white p-0">
-            <div>
-                <h4 class="fw-semibold text-center text-md-start"> Admin Status</h4>
-            </div>
-            <div id="admins-card">
-                <canvas id="admins-chart"></canvas>
-            </div>
-        </div>
-
-        <div class=" bg-white  p-0">
-            <div>
-                <h4 class="fw-semibold text-center text-md-start"> Random Stats</h4>
-            </div>
-            <div id="random-card">
-                <canvas id="random-chart"></canvas>
+            <div id="admins-card" class="w-100">
             </div>
         </div>
     </div>
 </section>
-
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script src="<?= base_url('assets/js/admin/index.js') ?>"></script>
--- assets/js/admin/index.js
+++ assets/js/admin/index.js
@@ -1,104 +1,110 @@
-const date = new Date;
-let hours = date.getHours();
-let status = (hours < 12) ? "morning" : ((hours <= 18 && hours >= 12 ) ? "afternoon" : "evening");
+$(document).ready(function () {
 
-$('#greeting-card').html(`Good ` + status + `, <span class="nav-user-container"></span> 👋`);
+    const date = new Date;
+    let hours = date.getHours();
+    let status = (hours < 12) ? "morning" : ((hours <= 18 && hours >= 12 ) ? "afternoon" : "evening");
+    
+    $('#greeting-card').html(`Good ` + status + `, <span class="nav-user-container"></span> 👋`);
+    $.ajax({
+        type: "get",
+        url: `${location.origin}/user/fetch/active`,
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            Highcharts.chart('users-card', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Active Users',
+                    align: 'left'
+                },
 
-const ctx = $('#myChart');
-const usersCard = $('#users-chart');
-const adminsCard = $('#admins-chart');
-const randomCard = $('#random-chart');
-
-$.ajax({
-    type: "get",
-    url: `${location.origin}/user/fetch/active`,
-    dataType: "json",
-    success: function(response) {
-        console.log(response);
-        new Chart(usersCard, {
-            type: 'doughnut',
-            data: {
-                labels: [
-                    response[0].code,
-                    response[1].code,
-
-                ],
-                datasets: [{
-                    label: 'User Status',
+                accessibility: {
+                    point: {
+                        valueSuffix: '%'
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    name: 'Users',
+                    colorByPoint: true,
                     data: [
-                        response[0].count,
-                        response[1].count,
-                    ],
-                    backgroundColor: [
-                        'rgb(193, 37, 83)',
-                        'rgb(255, 99, 132)',
-                    ],
-                    hoverOffset: 4
+                        {
+                            name: response[0].code,
+                            y: parseInt(response[0].count)
+                        },
+                        {
+                            name: response[1].code,
+                            y: parseInt(response[1].count)
+                        }]
                 }]
-            },
-            options: {
-                maintainAspectRatio: false,
-            }
-        });
-    }
-});
+            });
 
-
-$.ajax({
-    type: "get",
-    url: `${location.origin}/admin/fetch/active`,
-    dataType: "json",
-    success: function(response) {
-        console.log(response);
-        new Chart(adminsCard, {
-            type: 'doughnut',
-            data: {
-                labels: [
-                    response[0].code,
-                    response[1].code,
-
-                ],
-                datasets: [{
-                    label: 'Admin Status',
+        }
+    });
+    
+    
+    $.ajax({
+        type: "get",
+        url: `${location.origin}/admin/fetch/active`,
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            Highcharts.chart('admins-card', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Active Admins',
+                    align: 'left'
+                },
+    
+                accessibility: {
+                    point: {
+                        valueSuffix: '%'
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true
+                    }
+                },
+                series: [{
+                    name: 'Admins',
+                    colorByPoint: true,
                     data: [
-                        response[0].count,
-                        response[1].count,
-                    ],
-                    backgroundColor: [
-                        'rgb(2, 117, 216)',
-                        'rgb(91, 192, 222)',
-                    ],
-                    hoverOffset: 4
+                        {
+                            name: response[0].code,
+                            y: parseInt(response[0].count)
+                        },
+                        {
+                            name: response[1].code,
+                            y: parseInt(response[1].count)
+                        }]
                 }]
-            },
-            options: {
-                maintainAspectRatio: false,
-            }
-        });
-    }
-});
-
-
-new Chart(randomCard, {
-    type: 'doughnut',
-    data: {
-        labels: [
-            'Red',
-            'Blue',
-            'Yellow'
-        ],
-        datasets: [{
-            label: 'My First Dataset',
-            data: [300, 50, 100],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-    }
+            });
+        }
+    });
+        
 });
